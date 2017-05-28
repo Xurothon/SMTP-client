7151d6279f7d3cdf43ebb555710b8f862a6ccda0
Kazachonak Maksim <max.030298@mail.ru>
2017-05-28 19:39:57 +0300

    v1.0

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отправить сообщение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtUsername.Location = new System.Drawing.Point(66, 20);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(117, 25);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTo.Location = new System.Drawing.Point(53, 23);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(117, 25);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPassword.Location = new System.Drawing.Point(66, 56);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(117, 25);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Логин";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtUsername);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(209, 94);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Данные отправителя:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Пароль";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.txtTo);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Location = new System.Drawing.Point(245, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(209, 94);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Данные получателя:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Email";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtMessage);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox3.Location = new System.Drawing.Point(12, 129);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(209, 202);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Текст сообщения:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtMessage.Location = new System.Drawing.Point(9, 19);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMessage.Size = new System.Drawing.Size(185, 177);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox1);
+            this.groupBox4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox4.Location = new System.Drawing.Point(245, 129);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(209, 235);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Диалог с сервером:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(185, 210);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 379);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "SMTP-клиент";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net.Security;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connection connect = new Connection();
+            connect.connnection();
+            connect.authLogin(txtUsername.Text, txtPassword.Text);
+            connect.makeMail(txtTo.Text);
+            connect.sendMail(txtMessage.Text);
+            connect.closeConnection();
+            richTextBox1.Text = Addition.getNeedStr();
+        }
+
+    }
+
+    public class Addition
+    {
+        private static string needStr = "Диалог с сервером";
+        private Addition() { }
+
+        public static void textS(string str)
+        {
+            needStr += "\nS: " + str;
+        }
+
+        public static void textC(string str)
+        {
+            needStr += "\nC: " + str;
+        }
+
+        public static string getNeedStr()
+        {
+            return needStr;
+        }
+    }
+
+    public class Connection
+    {
+        private TcpClient client;
+        private SslStream sslStream;
+        private byte[] buffer = new byte[2048];
+        int bytes = -1;
+
+        public Connection()
+        {
+            this.client = new TcpClient();
+            client.Connect("smtp.gmail.com", 465);
+            this.sslStream = new SslStream(client.GetStream());
+        }
+
+        public void connnection()
+        {
+            
+            sslStream.AuthenticateAsClient("smtp.gmail.com");
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+            sslStream.Write(Encoding.UTF8.GetBytes("EHLO gmail.com" + "\r\n"));
+            Addition.textC("EHLO gmail.com");
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+        }
+
+        public void authLogin(string userName, string password)
+        {
+            sslStream.Write(Encoding.UTF8.GetBytes("AUTH LOGIN" + "\r\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC("AUTH LOGIN");
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+            string mail = Convert.ToBase64String(Encoding.UTF8.GetBytes(userName));
+            byte[] mail2 = Encoding.UTF8.GetBytes(mail + "\r\n");
+            sslStream.Write(mail2);
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC(Convert.ToBase64String(Encoding.UTF8.GetBytes(userName)));
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+            mail = Convert.ToBase64String(Encoding.UTF8.GetBytes(password));
+            mail2 = Encoding.UTF8.GetBytes(mail + "\r\n");
+            sslStream.Write(mail2);
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC(Convert.ToBase64String(Encoding.UTF8.GetBytes(password)));
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+        }
+
+        public void makeMail(string mailTo)
+        {
+            sslStream.Write(Encoding.UTF8.GetBytes("MAIL FROM: " + "<>" + "\r\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC("MAIL FROM: " + "<>");
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+            sslStream.Write(Encoding.UTF8.GetBytes("RCPT TO: " + "<" + mailTo + ">" + "\r\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC("RCPT TO: " + "<" + mailTo + ">");
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+        }
+
+        public void sendMail(string message)
+        {
+            sslStream.Write(Encoding.UTF8.GetBytes("DATA" + "\r\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC("DATA");
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+            sslStream.Write(Encoding.UTF8.GetBytes(message + "\n.\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC(message);
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));            
+        }
+
+        public void closeConnection()
+        {
+            sslStream.Write(Encoding.UTF8.GetBytes("QUIT" + "\r\n"));
+            bytes = sslStream.Read(buffer, 0, buffer.Length);
+            Addition.textC("QUIT");
+            Addition.textS(Encoding.UTF8.GetString(buffer, 0, bytes));
+        }
+        
+    }
+}
